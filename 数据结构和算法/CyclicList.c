7ae9104ad8c2558d6bf97ef25b40fddedea5c12b
CrazyJugg <CrazyJugg>
2018-03-24 16:57:44 +0800

    secend

--- 数据结构和算法/CyclicList.c
+++ 数据结构和算法/CyclicList.c
@@ -4,8 +4,24 @@
 //就使整个单链表形成一个环，这种头尾相接的单链表成为单循环链表。
 //其实虚幻链表的单链表的主要差异就在于循环的判断空链表的条件上，
 //原来判断head->next是否为null，现在则是head->next是否等于head
+
+//链表存储结构的定义
+typedef struct CLinkList
+{
+	int Data;
+	struct CLinkList *next;
+}node;
+
+//操作
+
+//初始化循环链表
+
+
+
+
 int main(void )
 {
+	
 	system("pause");
 	return 0;
 }
